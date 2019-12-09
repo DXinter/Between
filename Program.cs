9c13969b23d009a67ec21a78d878204897d9e7ab
DXinter <58430974+DXinter@users.noreply.github.com>
2019-12-09 19:08:24 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Between
+{
+    /*
+     * функция int[] Between(int a, int b), которая возвращает массив int[] с числами между a и b
+     */
+    class Program
+    {
+        static void Main()
+        {
+            Read read = new Read();
+            int a, b;
+            Set(out a, out b);
+            List<int> result = result = read.Between(a, b);
+            Results(result);
+
+        }
+
+        private static void Results(List<int> result)
+        {
+            Console.WriteLine("Numbers in the indicated interval: ");
+            for (int i = 0; i < result.Count; i++)
+                Console.WriteLine($"{result[i]}");
+        }
+
+        private static void Set(out int a, out int b)
+        {
+            Console.WriteLine("Enter first number:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            b = Convert.ToInt32(Console.ReadLine());
+        }
+       
+    }
+}
